c75cec2cab6e8894cd6dd6e83a59825de9770043
Daniel Hideki <daniel.hideki@vitaebrasil.com.br>
2015-05-15 13:06:16 -0300

    Adição de endereço para acessar o openshift e correção do pom.xml

--- src/main/java/com/project/Server.java
+++ src/main/java/com/project/Server.java
@@ -1,7 +1,9 @@
 package com.project;
 
 import static spark.Spark.get;
-
+import static spark.Spark.setIpAddress;
+import static spark.Spark.setPort;
+import static spark.Spark.staticFileLocation;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -13,9 +15,13 @@ import spark.Route;
 
 
 public class Server {
+	private static final String IP_ADDRESS = System.getenv("OPENSHIFT_DIY_IP") != null ? System.getenv("OPENSHIFT_DIY_IP") : "localhost";
+    private static final int PORT = System.getenv("OPENSHIFT_DIY_PORT") != null ? Integer.parseInt(System.getenv("OPENSHIFT_DIY_PORT")) : 8080;
 
 	public static void main(String[] args) {
-		
+		setIpAddress(IP_ADDRESS);
+        setPort(PORT);
+        staticFileLocation("/public/");
 		final MusicList list = new MusicList();
 		
 		list.addMusic(new Music("Note:Capo in the 3rd -- [G] How many special people change [F] How many lives...", new Specification("oasis", "champagnesupernova")));
